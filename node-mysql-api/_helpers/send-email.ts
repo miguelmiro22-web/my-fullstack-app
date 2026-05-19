0115b2df49d4cfcdd57abc4a759a918858d4f8c4
miguelmiro22-web <miguelmiro22@gmail.com>
2026-05-19 08:04:33 +0800

    Update send-email.ts

--- node-mysql-api/_helpers/send-email.ts
+++ node-mysql-api/_helpers/send-email.ts
@@ -1,14 +1,15 @@
 import nodemailer from 'nodemailer';
 
 export default async function sendEmail({ to, subject, html, from = process.env.EMAIL_FROM }: any) {
-    const transporter = nodemailer.createTransport({
-        host: process.env.SMTP_HOST || 'smtp-relay.brevo.com',
-        port: parseInt(process.env.SMTP_PORT || '587'),
-        auth: {
-            user: process.env.SMTP_USER || '',
-            pass: process.env.SMTP_PASS || '',
-        },
-        tls: { rejectUnauthorized: false }
-    });
+        const transporter = nodemailer.createTransport({
+            host: process.env.SMTP_HOST || 'smtp-relay.brevo.com',
+            port: 465,
+            secure: true,   // <-- add this line
+            auth: {
+                user: process.env.SMTP_USER || '',
+                pass: process.env.SMTP_PASS || '',
+            },
+            tls: { rejectUnauthorized: false }
+        });
     await transporter.sendMail({ from, to, subject, html });
 }
