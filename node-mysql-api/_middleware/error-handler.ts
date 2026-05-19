8117e942282a48d98d8ac2d42ac8ed67419b01af
miguelmiro22-web <miguelmiro22@gmail.com>
2026-05-19 09:11:25 +0800

    Update error-handler.ts

--- node-mysql-api/_middleware/error-handler.ts
+++ node-mysql-api/_middleware/error-handler.ts
@@ -1,11 +1,16 @@
 import type { Request, Response, NextFunction } from 'express';
+import { UnauthorizedError } from 'express-jwt';
 
 export default function errorHandler(
-    err: Error | string,
+    err: any,
     req: Request,
     res: Response,
     next: NextFunction
 ): Response | void {
+    if (err instanceof UnauthorizedError) {
+        return res.status(401).json({ message: 'Unauthorized' });
+    }
+
     if (typeof err === 'string') {
         const is404 = err.toLowerCase().endsWith('not found');
         const statusCode = is404 ? 404 : 400;
