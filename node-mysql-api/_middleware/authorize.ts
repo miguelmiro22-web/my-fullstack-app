afbd2f8204cbe05767cde48f5c3ad36b4e8006e8
miguelmiro22-web <miguelmiro22@gmail.com>
2026-05-19 09:57:40 +0800

    Update authorize.ts

--- node-mysql-api/_middleware/authorize.ts
+++ node-mysql-api/_middleware/authorize.ts
@@ -7,7 +7,24 @@ export default function authorize(roles: any = []) {
     if (typeof roles === 'string') {
         roles = [roles];
     }
-
+    return [
+    (req: any, res: any, next: any) => {
+        console.log('Auth header received:', req.headers.authorization ? 'YES' : 'NO');
+        next();
+    },
+    expressjwt({ secret, algorithms: ['HS256'] }),
+    async (req: any, res: any, next: any) => {
+        const account = await db.Account.findByPk(req.auth.id);
+        if (!account || (roles.length && !roles.includes(account.role))) {
+            return res.status(401).json({ message: 'Unauthorized' });
+        }
+        req.auth.role = account.role;
+        const refreshTokens = await account.getRefreshTokens();
+        req.auth.ownsToken = (token: any) => !!refreshTokens.find((x: any) => x.token === token);
+        next();
+    }
+];
+    
     return [
         expressjwt({ secret, algorithms: ['HS256'] }),
         async (req: any, res: any, next: any) => {
